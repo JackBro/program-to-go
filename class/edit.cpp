7322fd2a0bde4213dab7606971788dc5558b5f83
Frank Holler <holler.frank@gmail.com>
2013-04-01 03:28:23 +0200

    Hinzufügen von Button und Edit

---
+++ class/edit.cpp
@@ -0,0 +1,26 @@
+#include "edit.h"
+
+edit_c::edit_c(HWND hWnd,int left, int top, int width, int height):initcontrols_c()
+{
+   char * ccc = new char[5000];
+   char c[] = "this is";
+   memcpy(ccc,&c[0],strlen(c));
+   ccc[strlen(c)] = 0;
+   Wnd = CreateWindowEx(WS_EX_CLIENTEDGE,
+                      "edit",
+                      NULL,
+                      WS_VISIBLE | WS_CHILD | ES_LEFT,
+                      left,
+                      top,
+                      width,
+                      height,
+                      hWnd,
+                      NULL,
+                      NULL,
+                      NULL);    //ctor
+}
+
+edit_c::~edit_c()
+{
+    //dtor
+}
