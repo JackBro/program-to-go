6579bfd0e76e2efb87a436cde10651b81291f4d4
Frank Holler <holler.frank@gmail.com>
2013-04-03 21:37:39 +0200

    neue dropdownlist eingefügt,sort ist optional

---
+++ class/dropdownlist.cpp
@@ -0,0 +1,34 @@
+#include "dropdownlist.h"
+
+dropdownlist_c::dropdownlist_c(HWND hWnd, bool sort, int left, int top, int width, int height)
+{
+   DWORD Style = WS_VISIBLE | WS_CHILD | WS_BORDER | CBS_DROPDOWNLIST | CBS_AUTOHSCROLL | WS_TABSTOP;
+   if (sort) {
+     Style = Style | CBS_SORT;
+   }
+   Wnd = CreateWindow("COMBOBOX",
+                      NULL, Style,
+                      left,
+                      top,
+                      width,
+                      height,
+                      hWnd,
+                      NULL,
+                      NULL,
+                      NULL);
+}
+
+dropdownlist_c::~dropdownlist_c()
+{
+    //dtor
+}
+
+int dropdownlist_c::addEntry(char * text) {
+  SendMessage(Wnd,CB_ADDSTRING,0, (LPARAM)text);
+  return 0;
+}
+
+int dropdownlist_c::getCurSel() {
+  return SendMessage(Wnd,CB_GETCURSEL,0,0);
+}
+
